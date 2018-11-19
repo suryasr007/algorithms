c60a6e491c5e4dba9aebeabaa423be839d3ee231
Surya Teja <94suryateja@gmail.com>
2018-11-20 01:35:04 +0530

    problem 5

---
+++ Daily Coding Problem/5.py
@@ -0,0 +1,20 @@
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(f):
+    def left(a, b):
+        return a
+    return f(left)
+
+def cdr(f):
+    def right(a, b):
+        return b
+    return f(right)
+
+
+
+
+print(car(cons(2,3)))
+print(cdr(cons(2,3)))
